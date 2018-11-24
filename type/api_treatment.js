2cbb46c83f8c7c9bf6852e000d3dbd968236e430
bestcat1 <jakrawoot_1500@hotmail.com>
2018-11-24 19:37:52 +0700

    update 24/11/2561

--- type/api_treatment.js
+++ type/api_treatment.js
@@ -33,5 +33,50 @@ router.get('/show/:user',(req, res)=>{
     })
 })
 
+router.get('/show/:user/:cattle_id',(req, res)=>{
+    var user = req.params.user;
+    var cattle_id = req.params.cattle_id;
+
+    firebase.firebase().ref('treatment/'+user).orderByChild('id').equalTo(cattle_id).once('value',data=>{
+        res.json(data.val());
+    })
+})
+
+// router.get('/show/:user/:startDate/:endDate',(req, res)=>{
+//     var user = req.params.user;
+//     var startDate = req.params.startDate;
+//     var endDate = req.params.endDate;
+//     firebase.firebase().ref('treatment/'+user).orderByChild('datediagnose').startAt(startDate).endAt(endDate).once('value',data=>{
+//         res.json(data.val());
+//     })
+// })
+
+router.get('/showDrug/:user/:cattle_id/:count',(req, res)=>{
+    var user = req.params.user;
+    var cattle_id = req.params.cattle_id;
+    var count = req.params.count;
+    firebase.firebase().ref('treatment/drug/'+user+'/'+cattle_id).orderByChild('number_of_treatment').equalTo(Number(count)).once('value',data=>{
+        res.json(data.val());
+    })
+})
+
+router.post('/update/:user/:key',(req, res)=>{
+    var data = req.body;
+    var user = req.params.user;
+    var key = req.params.key;
+    firebase.firebase().ref('/treatment/'+user+'/'+key).update(data).then(data=>{
+        res.json('Sucess');
+    }, err=>{
+        res.json('Failed');
+    });
+})
+
+router.delete('/deleteDrug/:user/:cattle_id/:key',(req, res)=>{
+    var user = req.params.user;
+    var cattle_id = req.params.cattle_id;
+    var key = req.params.key;
+
+    firebase.firebase().ref('/treatment/drug/'+user+'/'+cattle_id+'/'+key).remove();
+})
 
 module.exports = router;
